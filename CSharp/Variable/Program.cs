acb2f075e629109c4dbf0218d6794ec3729e9f02
khkim3115 <khkim3115@gmail.com>
2022-12-12 21:47:22 +0900

    C# - FirstProgram, Variable
    FirstProgram
    Variable

---
+++ CSharp/Variable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variable
+{
+    internal class Program
+    {
+        int index = 7;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Human
+    {
+        int age;
+        float height;  //4바이트 실수형
+    }
+}
