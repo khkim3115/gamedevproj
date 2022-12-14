81d53508459a8df90e241e6fe6b0013d51b1c579
khkim3115 <khkim3115@gmail.com>
2022-12-14 21:45:19 +0900

    c# - Class Example

---
+++ CSharp/ClassExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human1 = new Human();
+            Human human2 = new Human();
+
+            human1.height = 200.0f;
+            human2.height = 150.0f;
+
+            human1.writeHeight();
+            human2.writeHeight();
+        }
+
+        class Human
+        {
+            public int age;
+            public float height;
+            public char sex;
+
+            public void writeHeight()
+            {
+                Console.WriteLine(height);
+            }
+        }
+
+    }
+}
--- CSharp/ClassObjectInstance/Program.cs
+++ CSharp/ClassObjectInstance/Program.cs
@@ -6,7 +6,32 @@ namespace ClassObjectInstance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Human human = new Human();
+
+            human.SayHeight();
+            human.SayAge();
+        }
+    }
+
+    class Human
+    {
+        private int age = 26;
+        private float height = 180;
+        private double weight = 70;
+        private bool isResting = true;
+
+
+        public void SayAge()
+        {
+            Console.WriteLine(age);
+        }
+        public void SayHeight()
+        {
+            Console.WriteLine(height);
         }
     }
 }
+
+//Class: 객체타입
+//Object: 객체: class타입으로 메모리공간을 확보한것
+//Instance: 객체(데이터가 할당된 경우)
