4e4db655ad0a69d644af6db6538fe06e884cf5e1
khkim3115 <khkim3115@gmail.com>
2022-12-15 21:41:51 +0900

    C# - Operators

---
+++ CSharp/Operators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 14.0f;
+            float b = 2.0f;
+            float c = 0.0f;
+
+            c = a + b;
+            Console.WriteLine(c);
+
+            c = b - a;
+            Console.WriteLine(c);
+
+            c = a / b;
+            Console.WriteLine(c);
+
+            c=a % b;
+            Console.WriteLine(c);
+
+            //증감연산자
+
+            //증가
+            c++;
+
+            //감소
+            c--;
+
+
+
+
+        }
+    }
+}
